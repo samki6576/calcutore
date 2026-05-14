917ca78f33b81340563ac3f1acf10bbd092c7a9b
Samra <hismm8690@gmail.com>
2026-05-13 22:02:22 -0700

    add

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,17 @@ export default defineConfig({
   server: {
     port: 5173,
     open: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  define: {
+    'process.env': {}
   }
 })
